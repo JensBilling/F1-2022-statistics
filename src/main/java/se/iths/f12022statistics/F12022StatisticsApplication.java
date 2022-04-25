0037eed2523e51964109a5bd786c076f2413a630
Jens Billing <jensis92@hotmail.com>
2022-04-25 18:01:04 +0200

    Initial commit

---
+++ src/main/java/se/iths/f12022statistics/F12022StatisticsApplication.java
@@ -0,0 +1,13 @@
+package se.iths.f12022statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F12022StatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F12022StatisticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/f12022statistics/F12022StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.f12022statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F12022StatisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
