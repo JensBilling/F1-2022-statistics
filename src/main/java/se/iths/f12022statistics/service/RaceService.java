5d3ba80e704623c356f5404e84d99be129e62c2d
jonas.granbom <jonasgranbom@hotmail.com>
2022-04-27 14:21:08 +0200

    Changes by Jens

--- src/main/java/se/iths/f12022statistics/service/RaceService.java
+++ src/main/java/se/iths/f12022statistics/service/RaceService.java
@@ -2,7 +2,20 @@ package se.iths.f12022statistics.service;
 
 
 import org.springframework.stereotype.Service;
+import se.iths.f12022statistics.entity.Race;
+import se.iths.f12022statistics.repository.RaceRepository;
 
 @Service
 public class RaceService {
+
+    private final RaceRepository raceRepository;
+
+    public RaceService(RaceRepository raceRepository) {
+        this.raceRepository = raceRepository;
+    }
+
+    public Race addNewRace(Race race) {
+        return raceRepository.save(race);
+
+    }
 }
