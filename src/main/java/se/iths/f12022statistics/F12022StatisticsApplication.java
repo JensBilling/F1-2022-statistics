3f8ae537ab0c4d012722ed7a7fae63f084e31737
jonas.granbom <jonasgranbom@hotmail.com>
2022-04-27 09:49:49 +0200

    Testing some weird db-shizznizz

--- src/main/java/se/iths/f12022statistics/F12022StatisticsApplication.java
+++ src/main/java/se/iths/f12022statistics/F12022StatisticsApplication.java
@@ -36,6 +36,46 @@ public class F12022StatisticsApplication {
     // Generate data at start up
     // Method to autofill database with data if empty
     public void fillDatabase(){
+        //RedBull
+        teamRepository.save(new Team("RedBull Racing", "RedBull power unit",
+                Stream.of(new Driver("MAX VERSTAPPEN", 23), new Driver("SERGIO PEREZ", 32)).collect(Collectors.toList()),
+                new Boss("CHRISTIAN HORNER", 48, 17)));
+        //Mercedes
+        teamRepository.save(new Team("Mercedes", "Mercedes powertrains",
+                Stream.of(new Driver("GEORGE RUSSELL", 24), new Driver("LEWIS HAMILTON", 37)).collect(Collectors.toList()),
+                new Boss("TOTO WOLFF", 50, 9)));
+        //McLaren
+        teamRepository.save(new Team("McLaren Racing", "Mercedes powertrains",
+                Stream.of(new Driver("LANDO NORRIS", 22), new Driver("DANIEL RICCIARDO", 32)).collect(Collectors.toList()),
+                new Boss("ZAK BROWN", 50, 5)));
+        //Ferrari
+        teamRepository.save(new Team("Ferrari", "Ferrari power unit",
+                Stream.of(new Driver("CARLOS SAINZ JR", 24), new Driver("CHARLES LECLERC", 24)).collect(Collectors.toList()),
+                new Boss("MATTIA BINOTTO", 52, 3)));
+        //Haas
+        teamRepository.save(new Team("Haas F1 Team", "Ferrari power unit",
+                Stream.of(new Driver("KEVIN MAGNUSSEN", 29), new Driver("MICK SCHUMACHER ", 23)).collect(Collectors.toList()),
+                new Boss("GÜNTHER STEINER", 57, 8)));
+        //Aston Martin
+        teamRepository.save(new Team("Aston Martin", "Mercedes powertrains",
+                Stream.of(new Driver("SEBASTIAN VETTEL", 34), new Driver("LANCE STROLL", 23)).collect(Collectors.toList()),
+                new Boss("MIKE KRACK", 50, 7)));
+        //Alpha Tauri
+        teamRepository.save(new Team("Alpha Tauri", "RedBull power unit",
+                Stream.of(new Driver("PIERRE GASLY", 26), new Driver("YUKI TSUNODA", 21)).collect(Collectors.toList()),
+                new Boss("FRANZ TOST", 66, 16)));
+        //Alfa Romeo
+        teamRepository.save(new Team("Alpha Romeo", "Ferrari power unit",
+                Stream.of(new Driver("VALTTERI BOTTAS", 32), new Driver("GUANYU ZHOU", 22)).collect(Collectors.toList()),
+                new Boss("FRÉDÉRIC VASSEUR", 53, 5)));
+        //Williams
+        teamRepository.save(new Team("Williams Racing", "Mercedes powertrains",
+                Stream.of(new Driver("ALEXANDER ALBON", 26), new Driver("NICHOLAS LATIFI", 26)).collect(Collectors.toList()),
+                new Boss("JOST CAPITO", 63, 1)));
+        //Alpine
+        teamRepository.save(new Team("Alpine F1", "Renault F1",
+                Stream.of(new Driver("ESTEBAN OCON", 25), new Driver("FERNANDO ALONSO", 40)).collect(Collectors.toList()),
+                new Boss("OTMAR SZAFNAUER", 63, 0)));
 
 
 
