f8b06f25f43c7d173cae8a99356bd80b973cacac
Jens Billing <jensis92@hotmail.com>
2022-05-02 22:36:27 +0200

    Changes by jonas

--- src/main/java/se/iths/f12022statistics/entity/Role.java
+++ src/main/java/se/iths/f12022statistics/entity/Role.java
@@ -10,6 +10,9 @@ import javax.persistence.*;
 @Entity
 @Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     @Column(length = 60)
--- src/main/java/se/iths/f12022statistics/repository/UserRepository.java
+++ src/main/java/se/iths/f12022statistics/repository/UserRepository.java
@@ -1,9 +1,25 @@
 package se.iths.f12022statistics.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import se.iths.f12022statistics.entity.User;
 
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+
 }
