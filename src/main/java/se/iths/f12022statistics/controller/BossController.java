ac1f9b49461c34712e8b81c92c7584ef244de099
Jens Billing <jensis92@hotmail.com>
2022-05-04 22:00:01 +0200

    Added endpoint to get all bosses

--- src/main/java/se/iths/f12022statistics/controller/BossController.java
+++ src/main/java/se/iths/f12022statistics/controller/BossController.java
@@ -1,9 +1,25 @@
 package se.iths.f12022statistics.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.iths.f12022statistics.entity.Boss;
+import se.iths.f12022statistics.service.BossService;
 
 @RestController
-@RequestMapping("bosses")
+@RequestMapping("api/bosses")
 public class BossController {
+
+    private final BossService bossService;
+
+    public BossController(BossService bossService) {
+        this.bossService = bossService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Boss>> getAllBosses(){
+        return ResponseEntity.ok(bossService.getAllBosses());
+    }
+
 }
--- src/main/java/se/iths/f12022statistics/service/BossService.java
+++ src/main/java/se/iths/f12022statistics/service/BossService.java
@@ -1,8 +1,21 @@
 package se.iths.f12022statistics.service;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import se.iths.f12022statistics.entity.Boss;
+import se.iths.f12022statistics.repository.BossRepository;
 
 @Service
 public class BossService {
+
+    private final BossRepository bossRepository;
+
+    public BossService(BossRepository bossRepository) {
+        this.bossRepository = bossRepository;
+    }
+
+    public Iterable<Boss> getAllBosses() {
+        return bossRepository.findAll();
+    }
 }
