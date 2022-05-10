f1073e76a5613f4a788be424e7b11702fd1fc18c
jonas.granbom <jonasgranbom@hotmail.com>
2022-05-10 11:05:42 +0200

    Added exceptionhandling for DriverService

--- src/main/java/se/iths/f12022statistics/service/DriverService.java
+++ src/main/java/se/iths/f12022statistics/service/DriverService.java
@@ -2,13 +2,17 @@ package se.iths.f12022statistics.service;
 
 
 import org.springframework.stereotype.Service;
+import se.iths.f12022statistics.entity.Boss;
 import se.iths.f12022statistics.entity.Driver;
 import se.iths.f12022statistics.entity.Team;
 import se.iths.f12022statistics.repository.DriverRepository;
 import se.iths.f12022statistics.repository.TeamRepository;
 import se.iths.f12022statistics.responsehandling.DeleteDriverFromDatabaseWithTeamRelationException;
+import se.iths.f12022statistics.responsehandling.EntityAlreadyExistsException;
+import se.iths.f12022statistics.responsehandling.NotFoundInDatabaseException;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
 @Service
 public class DriverService {
@@ -16,12 +20,18 @@ public class DriverService {
     private DriverRepository driverRepository;
     private TeamRepository teamRepository;
 
-    public DriverService(DriverRepository driverRepository, TeamRepository teamRepository){
+    public DriverService(DriverRepository driverRepository, TeamRepository teamRepository) {
         this.driverRepository = driverRepository;
         this.teamRepository = teamRepository;
     }
 
-    public Driver addNewDriver(Driver driver){
+    public Driver addNewDriver(Driver driver) {
+        Iterable<Driver> foundDriver = driverRepository.findAll();
+        for (Driver dbDriver : foundDriver) {
+            if (dbDriver.getName().equals(driver.getName())) {
+                throw new EntityAlreadyExistsException("That driver already exists in the database.");
+            }
+        }
         driverRepository.save(driver);
         return driver;
     }
@@ -31,23 +41,28 @@ public class DriverService {
     }
 
     public Driver getDriverById(Long id) {
-        Driver foundDriver = driverRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        return foundDriver;
+        Optional<Driver> foundDriver = retrieveDriverFromDB(id);
+        return foundDriver.get();
     }
 
     public void deleteDriverFromDatabase(Long id) {
         Iterable<Team> allTeams = teamRepository.findAll();
         for (Team team : allTeams) {
             for (Driver driver : team.getDrivers()) {
-                if (driver.getId() == id){
+                if (driver.getId() == id) {
                     throw new DeleteDriverFromDatabaseWithTeamRelationException("You can not delete a driver that is assigned to a team, remove team relation before you try again.");
                 }
             }
         }
-        Driver foundDriver = driverRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        driverRepository.delete(foundDriver);
+        Optional<Driver> foundDriver = retrieveDriverFromDB(id);
+        driverRepository.delete(foundDriver.get());
+    }
 
-
-
+    private Optional<Driver> retrieveDriverFromDB(Long id) {
+        Optional<Driver> foundDriver = driverRepository.findById(id);
+        if (foundDriver.isEmpty()) {
+            throw new NotFoundInDatabaseException("Driver with that id was not found in the database.");
+        }
+        return foundDriver;
     }
 }
